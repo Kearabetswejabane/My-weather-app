a815346b02ba9fe32e47541a4fca29903e8a5ffb
Kearabetswejabane <keahjabane@gmail.com>
2024-04-22 21:44:22 +0200

    Added icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_Url}" class="weather-app-icon" />`;
   timeElement.innerHTML = formatDate(date);
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
