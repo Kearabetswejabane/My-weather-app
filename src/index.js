f33103a86a20aba5904af53ca1ae9a98d917a0f6
Kearabetswejabane <keahjabane@gmail.com>
2024-04-22 19:27:31 +0200

    Updated temperature details

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = math.round(temperature);
   cityElement.innerHTML = response.data.city;
 }
